6591535a4a4793b9083305174b30f0842e7c8814
valerianroottea <54489241+valerianroottea@users.noreply.github.com>
2022-01-16 14:16:32 -0500

    Added form for logging workouts

---
+++ src/game.js
@@ -0,0 +1,7 @@
+function openForm() {
+  document.getElementById("workout-form").style.display = "block";
+}
+
+function closeForm() {
+  document.getElementById("workout-form").style.display = "none";
+}
