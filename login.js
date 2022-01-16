eb0257a8a7f580417af3a777f16037c72ba438e0
Simon W <simon.wong@archonsystems.com>
2022-01-16 15:30:57 -0500

    moved js, css file and modifed login html

--- login.js
+++
@@ -1,9 +0,0 @@
-document.getElementById("signUpBtn").addEventListener("click", function(){
-        document.getElementById("loginForm").style.display = "none";
-        document.getElementById("registrationForm").style.display = "block";
-    })
-
-document.getElementById("registrationBtn").addEventListener("click",function(){
-        document.getElementById("loginForm").style.display = "block";
-        document.getElementById("registrationForm").style.display = "none";
-    })
