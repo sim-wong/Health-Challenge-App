c91329dd260c1a974de2607a4fb3effe65448ff6
valerianroottea <54489241+valerianroottea@users.noreply.github.com>
2022-01-16 19:41:04 -0500

    Added new mountain image, added zoom function

--- src/game.js
+++ src/game.js
@@ -5,3 +5,13 @@ function openForm() {
 function closeForm() {
   document.getElementById("workout-form").style.display = "none";
 }
+
+function zoomOut() {
+  let mountain = document.getElementById("mountain-img");
+  mountain.style.width = "700px";
+}
+
+function zoomIn() {
+  let mountain = document.getElementById("mountain-img");
+  mountain.style.width = "2100px";
+}
