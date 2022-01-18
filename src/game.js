47ff333f90bb81af57853cfc8bad19d1e2bb0efe
valerianroottea <54489241+valerianroottea@users.noreply.github.com>
2022-01-17 22:08:16 -0500

    Added navigation functionality to game page

--- src/game.js
+++ src/game.js
@@ -1,6 +1,6 @@
 let mountain = document.getElementById("mountain-img");
-
-let currentMountainPositionBottom = mountain.offsetTop;
+let positionLeft = 0;
+let positionBottom = 0;
 
 function openForm() {
   document.getElementById("workout-form").style.display = "block";
@@ -16,4 +16,92 @@ function zoomOut() {
 
 function zoomIn() {
   mountain.style.width = "2100px";
+}
+
+function moveUp() {
+  let currentPosition = newPosition();
+  console.log(currentPosition);
+
+  function newPosition() {
+    positionBottom -= 100;
+
+    if (positionBottom >= -1200) {
+      return positionBottom;
+    } else {
+      positionBottom = -1200;
+    }
+  }
+  if (
+    currentPosition >= -1200 &&
+    currentPosition <= 0 &&
+    currentPosition != undefined
+  ) {
+    mountain.style.bottom = positionBottom + "px";
+  }
+}
+
+function moveDown() {
+  let currentPosition = newPosition();
+  console.log(currentPosition);
+
+  function newPosition() {
+    positionBottom += 100;
+
+    if (positionBottom <= 0) {
+      return positionBottom;
+    } else {
+      positionBottom = 0;
+    }
+  }
+  if (
+    currentPosition >= -1200 &&
+    currentPosition <= 0 &&
+    currentPosition != undefined
+  ) {
+    mountain.style.bottom = positionBottom + "px";
+  }
+}
+
+function moveLeft() {
+  let currentPosition = newPosition();
+  console.log(currentPosition);
+
+  function newPosition() {
+    positionLeft += 100;
+
+    if (positionLeft <= 0) {
+      return positionLeft;
+    } else {
+      positionLeft = 0;
+    }
+  }
+  if (
+    currentPosition >= -1200 &&
+    currentPosition <= 0 &&
+    currentPosition != undefined
+  ) {
+    mountain.style.left = positionLeft + "px";
+  }
+}
+
+function moveRight() {
+  let currentPosition = newPosition();
+  console.log(currentPosition);
+
+  function newPosition() {
+    positionLeft -= 100;
+
+    if (positionLeft >= -1200) {
+      return positionLeft;
+    } else {
+      positionLeft = -1200;
+    }
+  }
+  if (
+    currentPosition >= -1200 &&
+    currentPosition <= 0 &&
+    currentPosition != undefined
+  ) {
+    mountain.style.left = positionLeft + "px";
+  }
 }
