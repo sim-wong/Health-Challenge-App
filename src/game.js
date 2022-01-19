a9942a323c1bff4dad5672b5903441fbd2623d2f
valerianroottea <54489241+valerianroottea@users.noreply.github.com>
2022-01-19 00:12:21 -0500

    Added sprite creation page HTML and CSS

--- src/game.js
+++ src/game.js
@@ -1,3 +1,23 @@
+// Sprite Tab
+
+function openTab(event) {
+  console.log(event);
+}
+
+let skinTab = document.getElementById("skin-tab");
+let hairTab = document.getElementById("hair-tab");
+let shirtTab = document.getElementById("shirt-tab");
+let pantsTab = document.getElementById("pants-tab");
+let extraTab = document.getElementById("extra-tab");
+
+skinTab.addEventListener("click", openTab);
+hairTab.addEventListener("click", openTab);
+shirtTab.addEventListener("click", openTab);
+pantsTab.addEventListener("click", openTab);
+extraTab.addEventListener("click", openTab);
+
+// Game page
+
 let mountain = document.getElementById("mountain-img");
 let positionLeft = 0;
 let positionBottom = 0;
