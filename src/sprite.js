eb1941d9f282f2180daf84d85d9b9bc114ebbffc
valerianroottea <54489241+valerianroottea@users.noreply.github.com>
2022-01-20 22:23:32 -0500

    Added functionality to sprite option tabs for Skin and Hair

--- src/sprite.js
+++ src/sprite.js
@@ -1,13 +1,38 @@
 // Sprite Tabs
 
-// adding an event listner to tabs
+// adding an event listner to tabs and sprite img options
 // tab id in HTML should be set to "tab-x" where x is
 // the number of the tab
+// tab div id should be set to "tab-div-x"
 
 let numberOfTabs = document
   .getElementById("tab-ul")
   .getElementsByTagName("li").length;
 
+function addOption(event) {
+  let optionSelected = event.target.id;
+  let optionSelectedPreview = document.getElementById(`${optionSelected}-img`);
+  let optionSelectedPreviewId = optionSelectedPreview.id;
+  optionSelectedPreviewId = optionSelectedPreviewId.slice(0, -4);
+  let tabDiv = optionSelected.substring(0, 9);
+  let tabDivOptions = document.querySelectorAll(`#${tabDiv} img`);
+  let numberOfOptions = tabDivOptions.length;
+
+  for (i = 0; i < numberOfOptions; i++) {
+    console.log(numberOfOptions);
+    let spriteOption = tabDivOptions[i].id;
+    let spriteOptionStyle = document.getElementById(`${spriteOption}-img`);
+    console.log(spriteOptionStyle);
+    console.log(spriteOptionStyle);
+    if (spriteOption === optionSelectedPreviewId) {
+      optionSelectedPreview.classList.add("active-option");
+      optionSelectedPreview.classList.remove("inactive-option");
+    } else {
+      spriteOptionStyle.classList.add("inactive-option");
+      spriteOptionStyle.classList.remove("active-option");
+    }
+  }
+}
 function openTab(event) {
   let targetId = event.target.id;
 
@@ -31,5 +56,15 @@ function openTab(event) {
 
 for (i = 1; i <= numberOfTabs; i++) {
   let tabId = `tab-${i}`;
+  let tabDiv = `tab-div-${i}`;
+  let tabDivOptions = document.querySelectorAll(`#${tabDiv} img`);
+  let numberOfOptions = tabDivOptions.length;
+
+  // evemt listener for sprite tabs
   document.getElementById(`${tabId}`).addEventListener("click", openTab);
+
+  // event listener for sprite options
+  for (t = 0; t < numberOfOptions; t++) {
+    tabDivOptions[t].addEventListener("click", addOption);
+  }
 }
