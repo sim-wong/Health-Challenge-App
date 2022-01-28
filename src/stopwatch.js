24f27041527f4d6eb990fe5bce1b02013cb62b47
valerianroottea <54489241+valerianroottea@users.noreply.github.com>
2022-01-28 09:09:35 -0500

    Added function to switch tools on Stopwatch page

--- src/stopwatch.js
+++ src/stopwatch.js
@@ -1,3 +1,28 @@
+// tool selector
+
+let stopwatch = document.getElementById("stopwatch-tool");
+let timer = document.getElementById("timer-tool");
+let counter = document.getElementById("counter");
+
+function stopwatchSelect() {
+  timer.classList.remove("highlight");
+  stopwatch.classList.add("highlight");
+  counter.innerHTML = "00:00:00";
+}
+
+function timerSelect() {
+  timer.classList.add("highlight");
+  stopwatch.classList.remove("highlight");
+  counter.innerHTML = "00:05:00";
+}
+
+stopwatch.addEventListener("click", stopwatchSelect);
+timer.addEventListener("click", timerSelect);
+
+//Stopwatch
+
+// time controls
+
 let start = document.getElementById("start");
 let pause = document.getElementById("pause");
 let reset = document.getElementById("reset");
